91974298363fad5dea740b368f13a11da6ccf16c
Joel Takvorian <joel.takvorian@qaraywa.net>
2016-05-23 22:02:22 +0200

    Implement RESTful API
    Implement routes ; still need to test, need to fix "todos"

--- rest-api/restServer.ts
+++ rest-api/restServer.ts
@@ -1,10 +1,15 @@
 import express = require('express');
 import bodyParser = require('body-parser');
+import UserPersistenceService from '../persistence/userPersistenceService';
 
 "use strict";
 
 export default class RestServer {
-    public constructor(port: number) {
+
+    private userPersistenceService: UserPersistenceService;
+
+    public constructor(port: number, userPersistenceService: UserPersistenceService) {
+        this.userPersistenceService = userPersistenceService;
         let app = express();
         app.use(bodyParser.json());
         app.listen(port);
@@ -14,7 +19,128 @@ export default class RestServer {
 
     private registerRoutes(app: express.Application) {
         // Register CRUDL routes
+        this.registerListAll(app);
+        this.registerGetByUsername(app);
+        this.registerCreate(app);
+        this.registerUpdate(app);
+        this.registerDelete(app);
+        this.registerCustomSearch(app);
+        this.registerSearchByCity(app);
+        this.registerSearchByGender(app);
+        // ... and so on
+    }
 
-        // TODO: hide password??
+    private registerListAll(app: express.Application) {
+        var self = this;
+        // full list here but we may want partial list, or paginated... depends on usage
+        app.get("/users", (req, res) => {
+            // TODO: hide passwords??
+            self.userPersistenceService.all()
+                .then(users => res.send(users))
+                .fail(reason => {
+                    console.error("Application error: " + reason.message);
+                    // TODO: more accurate error codes depending on error (503...)
+                    res.status(500).send(String(reason));
+                })
+                .done();
+        });
+    }
+
+    private registerGetByUsername(app: express.Application) {
+        var self = this;
+        app.get("/users/:userName", (req, res) => {
+            self.userPersistenceService.getByUsername(req.params.userName)
+                .then(user => {
+                    // TODO: hide passwords??
+                    if (user === null) {
+                        res.status(404).send("User " + req.params.userName + " not found");
+                    } else {
+                        res.send(user);
+                    }
+                })
+                .fail(reason => {
+                    console.error("Application error: " + reason.message);
+                    // TODO: more accurate error codes depending on error (503...)
+                    res.status(500).send(String(reason));
+                })
+                .done();
+        });
+    }
+
+    private registerCreate(app: express.Application) {
+        var self = this;
+        app.post("/users/", (req, res) => {
+            self.userPersistenceService.create(req.body)
+                // TODO: manage conflict, manage invalid body
+                .then(() => res.send("OK"))
+                .fail(reason => {
+                    console.error("Application error: " + reason.message);
+                    // TODO: more accurate error codes depending on error (503...)
+                    res.status(500).send(String(reason));
+                })
+                .done();
+        });
+    }
+
+    private registerUpdate(app: express.Application) {
+        var self = this;
+        app.post("/users/:userName", (req, res) => {
+            if (req.body.userName !== req.params.userName) {
+                // Bad request
+                res.status(400).send("The username cannot be changed.");
+                return;
+            }
+            self.userPersistenceService.update(req.body)
+                // TODO: manage user not found, manage invalid body
+                .then(() => res.send("OK"))
+                .fail(reason => {
+                    console.error("Application error: " + reason.message);
+                    // TODO: more accurate error codes depending on error (503...)
+                    res.status(500).send(String(reason));
+                })
+                .done();
+        });
+    }
+
+    private registerDelete(app: express.Application) {
+        var self = this;
+        app.delete("/users/:userName", (req, res) => {
+            self.userPersistenceService.delete(req.params.userName)
+                // TODO: manage user not found
+                .then(() => res.send("OK"))
+                .fail(reason => {
+                    console.error("Application error: " + reason.message);
+                    // TODO: more accurate error codes depending on error (503...)
+                    res.status(500).send(String(reason));
+                })
+                .done();
+        });
+    }
+
+    private searchAndAnswer(searchQuery, res) {
+        // TODO: protect against injections?
+        this.userPersistenceService.search(searchQuery)
+            .then(users => res.send(users))
+            .fail(reason => {
+                console.error("Application error: " + reason.message);
+                // TODO: more accurate error codes depending on error (503...)
+                res.status(500).send(String(reason));
+            })
+            .done();
+    }
+
+    private registerCustomSearch(app: express.Application) {
+        var self = this;
+        app.post("/searches/custom", (req, res) => self.searchAndAnswer(req.body, res));
+    }
+
+    private registerSearchByCity(app: express.Application) {
+        var self = this;
+        app.get("/searches/users/city/:city", (req, res) => self.searchAndAnswer({"user.location.city": req.params.city}, res));
+    }
+
+    private registerSearchByGender(app: express.Application) {
+        var self = this;
+        app.get("/searches/users/gender/:gender", (req, res) => self.searchAndAnswer({"user.gender": req.params.gender}, res));
     }
 }
